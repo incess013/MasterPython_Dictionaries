0bf31d91896470734ffaeb45b6d27290c8e30d89
Princess Rodiel <cssjhnnamae@gmail.com>
2024-10-07 08:43:59 +0800

    docs: adding universityDict.py

---
+++ universityDict.py
@@ -0,0 +1,49 @@
+universityDict1 = {
+    "ID" : "1",
+    "Name" : "University of the Philippines",
+    "Location" : "Quezon City",
+    "Established Year" : "1908",
+    "Type" : "Public",
+    "Website" : "www.up.edu.ph"
+}
+print(universityDict1)
+
+universityDict2 = {
+    "ID" : "2",
+    "Name" : "Ateneo de Manila University",
+    "Location" : "Quezon City",
+    "Established Year" : "1859",
+    "Type" : "Private",
+    "Website" : "www.ateneo.edu"
+}
+print(universityDict2)
+
+universityDict3 = {
+    "ID" : "3",
+    "Name" : "De La Salle University",
+    "Location" : "Manila",
+    "Established Year" : "1911",
+    "Type" : "Private",
+    "Website" : "www.dlsu.edu.ph"
+}
+print(universityDict3)
+
+universityDict4 = {
+    "ID" : "4",
+    "Name" : "University of Santo Tomas",
+    "Location" : "Manila",
+    "Established Year" : "1611",
+    "Type" : "Private",
+    "Website" : "www.ust.edu.ph"
+}
+print(universityDict4)
+
+universityDict5 = {
+    "ID" : "5",
+    "Name" : "Polytechnic University of the Philippines ",
+    "Location" : "Manila",
+    "Established Year" : "1904 ",
+    "Type" : "Public",
+    "Website" : "www.pup.edu.ph"
+}
+print(universityDict5)
