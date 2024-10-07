5dfee707cb3bbdeca9c3f5d5ffc76a8bcd4973ab
Princess Rodiel <cssjhnnamae@gmail.com>
2024-10-07 08:38:55 +0800

    docs: adding employeeDict.py

---
+++ employeeDict.py
@@ -0,0 +1,44 @@
+employeeDict1 = {
+    "ID" : "1",
+    "Name" : "John Doe",
+    "Department" : "Sales",
+    "Age" : "30",
+    "Email" : "john.doe@company.com"
+}
+print(employeeDict1)
+
+employeeDict2 = {
+    "ID" : "2",
+    "Name" : "Jane Smith",
+    "Department" : "Human Resources",
+    "Age" : "25",
+    "Email" : "jane.smith@company.com"
+}
+print(employeeDict2)
+
+employeeDict3 = {
+    "ID" : "3",
+    "Name" : "Mark Johnson",
+    "Department" : "IT",
+    "Age" : "40",
+    "Email" : "mark.johnson@company.com"
+}
+print(employeeDict3)
+
+employeeDict4 = {
+    "ID" : "4",
+    "Name" : "Lisa Wong",
+    "Department" : "Marketing",
+    "Age" : "28",
+    "Email" : "lisa.wong@company.com"
+}
+print(employeeDict4)
+
+employeeDict5 = {
+    "ID" : "5",
+    "Name" : "Paul McDonald",
+    "Department" : "Finance",
+    "Age" : "28",
+    "Email" : "paul.mcdonald@company.com"
+}
+print(employeeDict5)
