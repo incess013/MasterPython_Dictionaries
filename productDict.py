f0119571685fe9a4aa9598f58fac1b566da288ee
Princess Rodiel <cssjhnnamae@gmail.com>
2024-10-07 08:34:51 +0800

    docs: adding productDict.py

---
+++ productDict.py
@@ -0,0 +1,49 @@
+productDict1 = {
+    "ID": "1",
+    "Name": "Laptop",
+    "Category": "Electronics",
+    "Price": "750",
+    "Stock": "50",
+    "Supplier Email": "supplier1@gmail.com"
+}
+print(productDict1)
+
+productDict2 = {
+    "ID": "2",
+    "Name": "Desk Chair",
+    "Category": "Furniture",
+    "Price": "100",
+    "Stock": "200",
+    "Supplier Email": "supplier2@gmail.com"
+}
+print(productDict2)
+
+productDict3 = {
+    "ID": "3",
+    "Name": "Smartwatch",
+    "Category": "Electronics",
+    "Price": "200",
+    "Stock": "150",
+    "Supplier Email": "supplier3@gmail.com"
+}
+print(productDict3)
+
+productDict4 = {
+    "ID": "4",
+    "Name": "Notebook",
+    "Category": "Stationery",
+    "Price": "5",
+    "Stock": "500",
+    "Supplier Email": "supplier4@gmail.com"
+}
+print(productDict4)
+
+productDict5 = {
+    "ID": "5",
+    "Name": "Running Shoes",
+    "Category": "Apparel",
+    "Price": "80",
+    "Stock": "100",
+    "Supplier Email": "supplier5@gmail.com"
+}
+print(productDict5)
