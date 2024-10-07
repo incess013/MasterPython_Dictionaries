037fed05191e0f34c84c291af0fa0b26732621aa
Princess Rodiel <cssjhnnamae@gmail.com>
2024-10-07 08:46:10 +0800

    docs: adding restaurantDict.py

---
+++ restaurantDict.py
@@ -0,0 +1,50 @@
+restaurantDict1 = {
+    "ID" : "1",
+    "Name" : "Viking Luxury Buffet",
+    "Location" : "Pasay City",
+    "Cuisine Type" : "Buffet",
+    "Established Year" : "2011",
+    "Webiste or Contact" : "www.vikings.ph"
+}
+print(restaurantDict1)
+
+restaurantDict2 = {
+    "ID" : "2",
+    "Name" : "Antonio's Restaurant",
+    "Location" : "Tagaytay",
+    "Cuisine Type" : "Fine Dining",
+    "Established Year" : "2002",
+    "Webiste or Contact" : "www.antoniosrestaurant.ph"
+}
+print(restaurantDict2)
+
+restaurantDict3 = {
+    "ID" : "3",
+    "Name" : "Mesa Filipino Moderne",
+    "Location" : "Makati City",
+    "Cuisine Type" : "Filipino",
+    "Established Year" : "2009",
+    "Webiste or Contact" : "www.mesa.ph"
+}
+print(restaurantDict3)
+
+restaurantDict4 = {
+    "ID" : "4",
+    "Name" : "Manam Comfort Filipino",
+    "Location" : "Quezon City",
+    "Cuisine Type" : "Filipino",
+    "Established Year" : "2013",
+    "Webiste or Contact" : "www.manam.ph"
+}
+print(restaurantDict4)
+
+
+restaurantDict5 = {
+    "ID" : "5",
+    "Name" : "Ramen Nagi",
+    "Location" : "Various locations",
+    "Cuisine Type" : "Japanese",
+    "Established Year" : "2013",
+    "Webiste or Contact" : "www.ramennagi.com.ph"
+}
+print(restaurantDict5)
